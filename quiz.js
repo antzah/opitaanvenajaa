e3a5d402b54b127ea5748408ccc64cd7ee1faedf
Anssi Hautaviita <anssi.hautaviita@gmail.com>
2016-07-08 22:42:05 +0300

    Sound effects + Text to speech

--- quiz.js
+++ quiz.js
@@ -8,6 +8,11 @@ $(document).ready(function(){
   generateQuestion();
   var score = 0;
 
+  var right = new Audio("ding.mp3");
+  var wrong = new Audio("error.mp3");
+
+  right.mediaGroup = 'soundGroup';
+  wrong.mediaGroup = 'soundGroup';
 
   /* 
   * Grabs an object from sanat.js
@@ -17,7 +22,7 @@ $(document).ready(function(){
   function generateQuestion(){
 
     // Fade in effect to start
-    $("#sanadiv, #vaihtoehdotdiv").fadeIn(100);
+    $('#sanadiv, #vaihtoehdotdiv').animate({opacity: 100}, 100);
 
     var sanaArray = sanat[Math.floor(Math.random()*sanat.length)];
     
@@ -29,6 +34,8 @@ $(document).ready(function(){
     $("#choice4").text(sanaArray["vaihtoehdot"][3]);
     $("#vastaus").text(sanaArray["oikea"]);
 
+    $("#kuuntele").attr("onclick", "responsiveVoice.speak('" + sanaArray['sana'] + "', 'Russian Female');");
+
   }
 
   $(".choicediv").click(function(){
@@ -37,16 +44,19 @@ $(document).ready(function(){
     var oikea = $("#vastaus").text();
 
     if (selection == oikea){
+      var sound = new Howl({urls: ['ding.mp3']}).play();
       score++;
     }
 
     else {
+      var sound = new Howl({urls: ['error.mp3']}).play();
       score = 0;
     }
 
     $("#score").text("Pisteputki: " + score);
 
-    $("#sanadiv, #vaihtoehdotdiv").fadeOut(100);
+    $('#sanadiv, #vaihtoehdotdiv').animate({opacity: 0}, 100);
+
     setTimeout(function(){
       generateQuestion();
     }, 200);
